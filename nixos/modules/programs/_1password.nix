db9e4be6b7a6d3fbf43671304da82d3a1a73e3be
Philip Lykke Carlsen <plcplc@gmail.com>
2024-10-18 12:38:40 +0200

    Add users to the `onepassword-cli` group
    This commit adds an option `cliUsers` which when set adds
    `config.users.users.${user}.extraGroups = ["onepassword-cli"]`.
    
    This is an alternative to using `security.wrappers` to wrap the setgid
    executable in `/run/wrappers/bin/op`.
    
    Setting user group membership explicitly directly is preferable to
    relying on setgid wrappers because it lets you opt int to access by
    user.

--- nixos/modules/programs/_1password.nix
+++ nixos/modules/programs/_1password.nix
@@ -19,21 +19,46 @@ in
       package = lib.mkPackageOption pkgs "1Password CLI" {
         default = [ "_1password" ];
       };
-    };
-  };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
-    users.groups.onepassword-cli.gid = config.ids.gids.onepassword-cli;
-
-    security.wrappers = {
-      "op" = {
-        source = "${cfg.package}/bin/op";
-        owner = "root";
-        group = "onepassword-cli";
-        setuid = false;
-        setgid = true;
+      cliUsers = lib.mkOption {
+        default = [];
+        defaultText = "[]";
+        description = "Users that will have access to the 1password cli through being members of the 'onepassword-cli' group";
+        example = [ "root" "alice" ];
+        type = lib.types.listOf lib.types.str;
       };
     };
   };
+
+  config = lib.mkIf cfg.enable (
+    let
+      # Config that makes 'cliUsers' members of the 'openpassword-cli' group.
+      userGroupsConfig = lib.mkIf (cfg.cliUsers != []) {
+        users.users = builtins.foldl'
+                       (user : users : users // user)
+                       {}
+                       (builtins.map
+                         (username : {"${username}".extraGroups = ["onepassword-cli"];})
+                         cfg.cliUsers
+                       );
+      };
+    in
+    lib.mkMerge [
+      {
+        environment.systemPackages = [ cfg.package ];
+        users.groups.onepassword-cli.gid = config.ids.gids.onepassword-cli;
+
+        security.wrappers = {
+          "op" = {
+            source = "${cfg.package}/bin/op";
+            owner = "root";
+            group = "onepassword-cli";
+            setuid = false;
+            setgid = true;
+          };
+        };
+      }
+      userGroupsConfig
+      ]
+  );
 }
